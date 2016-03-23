9146f81c8f6aa03a58b6f51546f9c9753bcf34d8
janeeats <jane.vora@gmail.com>
2016-03-23 02:00:45 +0000

    starter code ready

--- script.js
+++ script.js
@@ -13,38 +13,9 @@ $(document).ready(function(){
     var operator = "";
     var display = $("#display");
     display.text("0");
-    $("#numbers a").not("#clear,#clearAll").click(function(){
-		number += $(this).text();
-		display.text(number);
-		testNumLength(number);
-    });
-    $("#operators a").not("#equals").click(function(){
-		operator = $(this).text();
-		newNumber = number;
-		number = "";
-		display.text(operator);
-    });
-    $("#clear,#clearAll").click(function(){
-		number = "";
-		display.text("0");
-		if ($(this).attr("id") === "clearAll") {
-			newNumber = "";
-		}
-    });
-    $("#equals").click(function(){
-		var answer;
-        if (operator === "+"){
-			answer = parseInt(number) + parseInt(newNumber);
-		} else if (operator === "-"){
-			answer = parseInt(newNumber) - parseInt(number);
-		} else if (operator === "/"){
-			answer = parseInt(newNumber) / parseInt(number);
-		} else if (operator === "*"){
-			answer = parseInt(number) * parseInt(newNumber);
-		}
-		display.text(answer);
-		testNumLength(answer);
-		number = "";
-		newNumber = "";
-    });
+
+	// write 4 click functions below:
+	
+	
+	
 });
